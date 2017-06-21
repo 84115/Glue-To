942eff8117353f9cc3e4c6142da8d2528f43c78a
James <hello@james-ball.co.uk>
2017-06-21 14:19:31 +0100

    Initial Pager Work

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { keys, head } from 'ramda'
 import stack from 'routes/stack'
 import persist from 'routes/persist'
 import edit from 'routes/edit'
+import paginate from 'routes/paginate'
 import ajax from 'routes/ajax'
 import ajaxv2 from 'routes/ajax-v2'
 import demo from 'routes/demo'
@@ -15,6 +16,7 @@ let routes = {
     "": { view: stack },
     "/stack": { view: stack },
     "/persist": { view: persist },
+    "/paginate": { view: paginate },
     "/ajax": { view: ajax },
     "/ajax-v2": { view: ajaxv2 },
     "/demo": { view: demo },
---
+++ src/routes/paginate.js
@@ -0,0 +1,7 @@
+import { paginate } from 'mythic/paginate'
+import { p } from 'mythic/markup'
+// import { compose, map, inc, length, range, init, append } from 'ramda'
+
+let el = node => p('pending')
+
+export default el
